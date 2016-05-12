28f16bb7d8100d943da41bc057feb8863e12e686
阳君 <937447974@qq.com>
2016-05-12 16:27:07 +0800

    pod 1.1.0

--- YJCocoa/YJCocoa/AppDelegate.h
+++ YJCocoa/YJCocoa/AppDelegate.h
@@ -2,6 +2,9 @@
 //  AppDelegate.h
 //  YJCocoa
 //
+//  HomePage:https://github.com/937447974/YJCocoa
+//  YJ技术支持群:557445088
+//
 //  Created by 阳君 on 16/5/11.
 //  Copyright © 2016年 YJ. All rights reserved.
 //
