a8b798e51b96971020249fb7240d9e760e42327d
阳君 <937447974@qq.com>
2016-11-03 18:01:49 +0800

    升级说明

--- Documentation/docset-installed.sh
+++ Documentation/docset-installed.sh
@@ -31,8 +31,9 @@ if [ -d ${docset_dest_path} ]; then
 fi
 
 # 更新记录
-osascript -e 'display notification "2016-11-03 | YJCocoa最低支持有iOS6升级到iOS7" with title "YJCocoa 5.0.0"'
+osascript -e 'display notification "2016-11-03 | YJCocoa最低支持由iOS6升级到iOS7" with title "YJCocoa 5.0.0"'
 osascript -e 'display notification "2016-11-03 | CoreData上线，支持并发操作、自动保存、前台保存、后台保存以及数据库版本升级" with title "YJCocoa 5.0.0"'
+osascript -e 'display notification "2016-11-03 | NavigationBar升级，支持自定义按钮" with title "YJCocoa 5.0.0"'
 
 
 
