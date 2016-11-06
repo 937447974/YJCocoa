853a4bbab0c89b0fff627852673fb7f7d7f3a0fd
阳君 <937447974@qq.com>
2016-11-06 15:39:41 +0800

    code terse

--- Cocoa/AppFrameworks/UIKit/TableViewManager/Suspension/YJUISuspensionCellView.m
+++ Cocoa/AppFrameworks/UIKit/TableViewManager/Suspension/YJUISuspensionCellView.m
@@ -6,7 +6,7 @@
 //  YJ技术支持群:557445088
 //
 //  Created by 阳君 on 16/8/23.
-//  Copyright © 2016年 YJ. All rights reserved.
+//  Copyright © 2016年 YJCocoa. All rights reserved.
 //
 
 #import "YJUISuspensionCellView.h"
--- Cocoa/AppServices/YJAppServices.h
+++ Cocoa/AppServices/YJAppServices.h
@@ -6,7 +6,7 @@
 //  YJ技术支持群:557445088
 //
 //  Created by 阳君 on 2016/11/3.
-//  Copyright © 2016年 YJ. All rights reserved.
+//  Copyright © 2016年 YJCocoa. All rights reserved.
 //
 
 // CoreData封装，支持并发操作、自动保存、前台保存、后台保存以及数据库版本升级
