d1fd08d4bee860e10fb91caefe992712ef4344ac
阳君 <937447974@qq.com>
2016-12-19 17:11:36 +0800

    NavigationRouter 框架建立

---
+++ Developer/AppFrameworks/UIKit/YJUINavigationRouter/YJUINavigationRouter/Main/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YJUINavigationRouter
+//
+//  Created by admin on 2016/12/19.
+//  Copyright © 2016年 YJCocoa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Developer/AppFrameworks/UIKit/YJUINavigationRouter/YJUINavigationRouter/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  YJUINavigationRouter
+//
+//  Created by admin on 2016/12/19.
+//  Copyright © 2016年 YJCocoa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
