81c21dd2f1a6a406826b2afdc3b57e49c8711c4a
阳君 <937447974@qq.com>
2016-12-20 16:46:09 +0800

    code terse

--- Cocoa/AppFrameworks/UIKit/NavigationBar/YJUINavigationBar.m
+++ Cocoa/AppFrameworks/UIKit/NavigationBar/YJUINavigationBar.m
@@ -2,7 +2,7 @@
 //  YJNavigationBar.m
 //  YJNavigationBar
 //
-//  HomePage:https://github.com/937447974/YJUIocoa
+//  HomePage:https://github.com/937447974/YJCocoa
 //  YJ技术支持群:557445088
 //
 //  Created by 阳君 on 16/7/6.
