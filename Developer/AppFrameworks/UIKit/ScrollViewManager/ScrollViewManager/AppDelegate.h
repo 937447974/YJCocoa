1a9206076a18c1c39b0c42887b644c797620f3fc
阳君 <937447974@qq.com>
2016-12-22 16:17:25 +0800

    ScrollViewManager项目建立

---
+++ Developer/AppFrameworks/UIKit/ScrollViewManager/ScrollViewManager/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ScrollViewManager
+//
+//  Created by admin on 2016/12/22.
+//  Copyright © 2016年 YJCocoa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Developer/AppFrameworks/UIKit/ScrollViewManager/ScrollViewManager/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ScrollViewManager
+//
+//  Created by admin on 2016/12/22.
+//  Copyright © 2016年 YJCocoa. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
