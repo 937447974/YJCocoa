b8acb05b04a64373655ba81d1543e9ea49a9ee8b
阳君 <937447974@qq.com>
2019-01-18 14:04:38 +0800

    feat: 更新头文件

--- Cocoa/AppFrameworks/Foundation/YJFoundation.h
+++ Cocoa/AppFrameworks/Foundation/YJFoundation.h
@@ -9,55 +9,22 @@
 //  Copyright © 2016年 YJCocoa. All rights reserved.
 //
 
-/** 面向切面编程*/
-#import "YJNSAspectOrientProgramming.h"
-
-/** 缓存工具*/
-#import "YJNSCache.h"
-
-// 日历工具，快速获取天、时、分等
-#import "YJNSCalendar.h"
-
-// Dictionary转Model
-#import "NSObject+YJNSDictionaryModel.h"
-
-// 获取应用内目录
-#import "YJNSDirectory.h"
-
-// NSFileManager扩展
-#import "YJNSFileManager.h"
-
-// http链接相关
-#import "YJNSHttp.h"
-
-// KVO
-#import "NSObject+YJNSKVO.h"
-
-// NSLog打印优化
-#import "YJNSLog.h"
+#import "YJNSAspectOrientProgramming.h"//面向切面编程
+#import "YJNSCache.h"                  //缓存工具
+#import "YJNSCalendar.h"               // 日历工具，快速获取天、时、分等
+#import "NSObject+YJNSDictionaryModel.h" // Dictionary转Model
+#import "YJNSDirectory.h"           // 获取应用内目录
+#import "YJNSFileManager.h"         // NSFileManager扩展
+#import "YJNSHttp.h"                // http链接相关
+#import "NSObject+YJNSKVO.h"        // KVO
+#import "YJNSLog.h"                 // NSLog打印优化
 #import "NSNotificationCenter+YJ.h" // 通知中心 block 回调
-
-// 线程安全执行Selector
-#import "NSObject+YJNSPerformSelector.h"
-
-// 路由器导航基础库
-#import "YJNSURLRouter.h"
-
-// 宏定义，快速将类转换为单例类
-#import "YJNSSingleton.h"
-
-// 交换方法
-#import "YJSwizzling.h"
-
-// 计时器
-#import "YJNSTimer.h"
-
-// URLEncode编码和URLDecode解码
-#import "YJNSURLCode.h"
-
-// 网络架构
-#import "YJNSURLSession.h"
-
-// 其他不可归类封装
-#import "YJNSFoundationOther.h"
-
+#import "YJNSFoundationOther.h"     // 其他不可归类封装
+#import "NSObject+YJNSPerformSelector.h"// 线程安全执行Selector
+#import "YJScheduler.h"    // 调度器
+#import "YJNSSingleton.h"  // 宏定义，快速将类转换为单例类
+#import "YJSwizzling.h"    // 交换方法
+#import "YJNSTimer.h"      // 计时器
+#import "YJNSURLCode.h"    // URLEncode编码和URLDecode解码
+#import "YJNSURLRouter.h"  // 路由器导航基础库
+#import "YJNSURLSession.h" // 网络架构
