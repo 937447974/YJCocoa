41dc52f86ee255352188de76f4378e3cc1a0c5dd
阳君 <937447974@qq.com>
2019-03-06 18:08:36 +0800

    feat:  block 代码注入解析

--- Cocoa/AppFrameworks/Foundation/CodeInject/YJCodeInject.m
+++ Cocoa/AppFrameworks/Foundation/CodeInject/YJCodeInject.m
@@ -12,6 +12,7 @@
 #import "YJCodeInject.h"
 #import <dlfcn.h>
 #import <mach-o/getsect.h>
+#import <mach-o/dyld.h>
 
 @implementation YJCodeInject
 
@@ -31,15 +32,17 @@
 
 + (void)executeBlockForKey:(NSString *)key {
     NSString *sectname = [NSString stringWithFormat:@"%@b", key?:@""];
-    Dl_info info;
-    dladdr((__bridge void *)[UIApplication.sharedApplication.delegate class], &info);
-    const uint64_t mach_header = (uint64_t)info.dli_fbase;
-    const struct section_64 *section = getsectbynamefromheader_64((void *)mach_header, "__YJCocoa", sectname.UTF8String);
-    if (section == NULL) return;
     int addrOffset = sizeof(struct YJCI_Block);
-    for (uint64_t addr = section->offset; addr < section->offset + section->size; addr += addrOffset) {
-        struct YJCI_Block entry = *(struct YJCI_Block *)(mach_header + addr);
-        !entry.block ?: entry.block();
+    uint32_t count = _dyld_image_count();
+    for (uint32_t i = 0 ; i < count; ++i) {
+        if (strstr(_dyld_get_image_name(i), "/private/var/") == NULL) continue;
+        const uint64_t mach_header = (uint64_t)_dyld_get_image_header(i);
+        const struct section_64 *section = getsectbynamefromheader_64((void *)mach_header, "__YJCocoa", sectname.UTF8String);
+        if (section == NULL) continue;
+        for (uint64_t addr = section->offset; addr < section->offset + section->size; addr += addrOffset) {
+            struct YJCI_Block entry = *(struct YJCI_Block *)(mach_header + addr);
+            !entry.block ?: entry.block();
+        }
     }
 }
 
