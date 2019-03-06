71a5b76a38ce73da3db52ae302e9420861750884
阳君 <937447974@qq.com>
2019-03-06 13:48:38 +0800

    style: 更新头文件说明

--- Cocoa/AppFrameworks/Foundation/Scheduler/Private/YJSchedulerIntercept.m
+++ Cocoa/AppFrameworks/Foundation/Scheduler/Private/YJSchedulerIntercept.m
@@ -1,8 +1,12 @@
 //
 //  YJSchedulerIntercept.m
-//  Pods-YJScheduler
+//  YJCocoa
+//
+//  HomePage:https://github.com/937447974/YJCocoa
+//  YJ技术支持群:557445088
 //
 //  Created by 阳君 on 2019/1/3.
+//  Copyright © 2019年 YJCocoa. All rights reserved.
 //
 
 #import "YJSchedulerIntercept.h"
--- Cocoa/AppFrameworks/Foundation/Scheduler/Private/YJSchedulerSubscribe.m
+++ Cocoa/AppFrameworks/Foundation/Scheduler/Private/YJSchedulerSubscribe.m
@@ -1,8 +1,12 @@
 //
 //  YJSchedulerSubscribe.m
-//  Pods
+//  YJCocoa
+//
+//  HomePage:https://github.com/937447974/YJCocoa
+//  YJ技术支持群:557445088
 //
 //  Created by 阳君 on 2018/12/29.
+//  Copyright © 2018年 YJCocoa. All rights reserved.
 //
 
 #import "YJSchedulerSubscribe.h"
