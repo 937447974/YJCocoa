45d27d44a1e8e5fe53c074dfb2250afba33d1f19
阳君 <937447974@qq.com>
2019-04-23 10:06:50 +0800

    fix: 代码注入支持 32 位系统

--- Cocoa/AppFrameworks/Foundation/CodeInject/YJCodeInject.m
+++ Cocoa/AppFrameworks/Foundation/CodeInject/YJCodeInject.m
@@ -13,12 +13,20 @@
 #import <mach-o/dyld.h>
 #import <mach-o/getsect.h>
 
+#if defined(__LP64__) && __LP64__
+#define yj_section section_64
+#define yj_getsectbynamefromheader getsectbynamefromheader_64
+#else
+#define yj_section section
+#define yj_getsectbynamefromheader getsectbynamefromheader
+#endif
+
 @implementation YJCodeInject
 
 + (void)executeFunctionForKey:(NSString *)key {
     NSString *sectname = [NSString stringWithFormat:@"%@f", key];
     uint64_t addrOffset = sizeof(struct YJCI_Function);
-    [self executeMatchForSectname:sectname completionHandler:^(const uint64_t mach_header, const struct section_64 *section) {
+    [self executeMatchForSectname:sectname completionHandler:^(const uint64_t mach_header, const struct yj_section *section) {
         for (uint64_t addr = section->offset; addr < section->offset + section->size; addr += addrOffset) {
             struct YJCI_Function entry = *(struct YJCI_Function *)(mach_header + addr);
             entry.function();
@@ -29,7 +37,7 @@
 + (void)executeBlockForKey:(NSString *)key {
     NSString *sectname = [NSString stringWithFormat:@"%@b", key];
     uint64_t addrOffset = sizeof(struct YJCI_Block);
-    [self executeMatchForSectname:sectname completionHandler:^(const uint64_t mach_header, const struct section_64 *section) {
+    [self executeMatchForSectname:sectname completionHandler:^(const uint64_t mach_header, const struct yj_section *section) {
         for (uint64_t addr = section->offset; addr < section->offset + section->size; addr += addrOffset) {
             struct YJCI_Block entry = *(struct YJCI_Block *)(mach_header + addr);
             !entry.block ?: entry.block();
@@ -37,7 +45,7 @@
     }];
 }
 
-+ (void)executeMatchForSectname:(NSString *)sectname completionHandler:(void (^)(const uint64_t mach_header, const struct section_64 *section))handler {
++ (void)executeMatchForSectname:(NSString *)sectname completionHandler:(void (^)(const uint64_t mach_header, const struct yj_section *section))handler {
     uint32_t count = _dyld_image_count();
     for (uint32_t i = 0 ; i < count; ++i) {
 #if TARGET_IPHONE_SIMULATOR
@@ -46,7 +54,7 @@
         if (strstr(_dyld_get_image_name(i), "/var/") == NULL) continue;
 #endif
         const uint64_t mach_header = (uint64_t)_dyld_get_image_header(i);
-        const struct section_64 *section = getsectbynamefromheader_64((void *)mach_header, "__YJCocoa", sectname.UTF8String);
+        const struct yj_section *section = yj_getsectbynamefromheader((void *)mach_header, "__YJCocoa", sectname.UTF8String);
         if (section == NULL) continue;
         handler(mach_header, section);
     }
