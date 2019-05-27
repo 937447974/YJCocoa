f924b56d913c487dbc619383e74aebab3c94126f
阳君 <937447974@qq.com>
2019-05-27 22:55:21 +0800

    feat: Directory 快速访问应用目录

---
+++ YJCocoa/Classes/AppFrameworks/Foundation/Directory/YJNSDirectory.swift
@@ -0,0 +1,51 @@
+//
+//  YJNSDirectory.swift
+//  Pods
+//
+//  Created by 阳君 on 2019/5/27.
+//
+
+import UIKit
+
+public func YJNSDirectoryS() -> YJNSDirectory {
+    return YJWeakSingleton(YJNSDirectory.self, "YJNSDirectory") as! YJNSDirectory
+}
+
+open class YJNSDirectory: NSObject {
+    
+    /// HomeDirectoryPath
+    public let homePath: String!
+    /// DocumentDirectoryPath
+    public let documentPath: String!
+    /// LibraryDirectoryPath
+    public let libraryPath: String!
+    /// CachesDirectoryPath
+    public let cachesPath: String!
+    /// TemporaryDirectoryPath
+    public let tempPath: String!
+    
+    /// HomeDirectoryURL
+    public let homeURL: URL!
+    /// DocumentDirectoryPath
+    public let documentURL: URL!
+    /// LibraryDirectoryPath
+    public let libraryURL: URL!
+    /// CachesDirectoryPath
+    public let cachesURL: URL!
+    /// TemporaryDirectoryPath
+    public let tempURL: URL!
+    
+    public override init() {
+        self.homePath = NSHomeDirectory()
+        self.documentPath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first!
+        self.libraryPath = NSSearchPathForDirectoriesInDomains(.libraryDirectory, .userDomainMask, true).first!
+        self.cachesPath = NSSearchPathForDirectoriesInDomains(.cachesDirectory, .userDomainMask, true).first!
+        self.tempPath = NSTemporaryDirectory()
+        self.homeURL = URL(fileURLWithPath: self.homePath)
+        self.documentURL = URL(fileURLWithPath: self.documentPath)
+        self.libraryURL = URL(fileURLWithPath: self.libraryPath)
+        self.cachesURL = URL(fileURLWithPath: self.cachesPath)
+        self.tempURL = URL(fileURLWithPath: self.tempPath)
+    }
+    
+}
