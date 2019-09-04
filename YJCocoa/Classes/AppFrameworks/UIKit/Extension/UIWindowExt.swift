dc45eddfc7c6729d7c20cb4f552567cc34045904
阳君 <937447974@qq.com>
2019-09-04 23:06:58 +0800

    feat: UIWindow 添加扩展，支持获取当前 UIWindow

---
+++ YJCocoa/Classes/AppFrameworks/UIKit/Extension/UIWindowExt.swift
@@ -0,0 +1,27 @@
+//
+//  UIWindowExt.swift
+//  YJCocoa
+//
+//  HomePage:https://github.com/937447974/YJCocoa
+//  YJ技术支持群:557445088
+//
+//  Created by 阳君 on 2019/9/4.
+//  Copyright © 2019 YJCocoa. All rights reserved.
+//
+
+import UIKit
+
+public extension UIWindow {
+    
+    /// 当前 UIWindow
+    static var current: UIWindow? {
+        let frontToBackWindows = UIApplication.shared.windows.reversed()
+        for window in frontToBackWindows {
+            if window.isKeyWindow, window.windowLevel >= .normal, window.screen == UIScreen.main, !window.isHidden && window.alpha > 0 {
+                return window
+            }
+        }
+        return nil
+    }
+    
+}
