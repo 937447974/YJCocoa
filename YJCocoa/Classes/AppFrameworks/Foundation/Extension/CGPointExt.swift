8e4383df9ca194c3b14ca2372c1ef383f4a34788
阳君 <937447974@qq.com>
2019-09-30 14:34:31 +0800

    feat: 增加 CGPoint 加减处理

---
+++ YJCocoa/Classes/AppFrameworks/Foundation/Extension/CGPointExt.swift
@@ -0,0 +1,24 @@
+//
+//  CGPointExt.swift
+//  YJCocoa
+//
+//  HomePage:https://github.com/937447974/YJCocoa
+//  YJ技术支持群:557445088
+//
+//  Created by 阳君 on 2019/9/30.
+//  Copyright © 2016-现在 YJCocoa. All rights reserved.
+//
+
+
+public extension CGPoint {
+    
+    static func + (lhs: CGPoint, rhs: CGPoint) -> CGPoint {
+        return CGPoint(x: lhs.x + rhs.x, y: lhs.y + rhs.y)
+    }
+    
+    static func - (lhs: CGPoint, rhs: CGPoint) -> CGPoint {
+        return CGPoint(x: lhs.x - rhs.x, y: lhs.y - rhs.y)
+    }
+    
+}
+
