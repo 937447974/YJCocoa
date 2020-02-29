fa755a7f4c80406aedc2f95146a42fb10d0c1c67
阳君 <937447974@qq.com>
2020-02-29 19:21:13 +0800

    feat: 添加 array 的高级用法

--- YJCocoa/Classes/AppFrameworks/Foundation/Extension/ArrayExt.swift
+++ YJCocoa/Classes/AppFrameworks/Foundation/Extension/ArrayExt.swift
@@ -11,10 +11,55 @@
 
 import UIKit
 
+public extension Array {
+    
+    /// 按个数分割数组
+    /// - parameter count: 每组的个数
+    func split(count: Int) -> [[Element]] {
+        guard self.count > count else {
+            return [self]
+        }
+        var reslult = [[Element]]()
+        var group = [Element]()
+        for item in self {
+            group.append(item)
+            if group.count == count {
+                reslult.append(group)
+                group = [Element]()
+            }
+        }
+        if group.count != 0 { reslult.append(group) }
+        return reslult
+    }
+    
+    /// 将数组转换为字典
+    /// - parameter keySelector: 获取字典的key
+    func groupBy<K>(_ keySelector: (Element) -> K) -> [K: [Element]] where K: Any{
+        var result = [K: [Element]]()
+        for item in self {
+            let k = keySelector(item)
+            var list = result[k] ?? [Element]()
+            list.append(item)
+            result[k] = list
+        }
+        return result
+    }
+    
+}
+
 public extension Array where Element == String {
     func object(at index: Int) -> Element {
         guard index < self.count else {
             return ""
+        }
+        return self[index]
+    }
+}
+
+public extension Array where Element == Int {
+    func object(at index: Int) -> Element {
+        guard index < self.count else {
+            return 0
         }
         return self[index]
     }
