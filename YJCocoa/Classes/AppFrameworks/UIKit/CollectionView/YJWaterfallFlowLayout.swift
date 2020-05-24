ad0a0c32b8f299ec62f0ecce118f6fb6d58c1048
阳君 <937447974@qq.com>
2020-05-24 22:13:02 +0800

    chore: 代码逻辑整理

--- YJCocoa/Classes/AppFrameworks/UIKit/CollectionView/YJWaterfallFlowLayout.swift
+++ YJCocoa/Classes/AppFrameworks/UIKit/CollectionView/YJWaterfallFlowLayout.swift
@@ -32,6 +32,7 @@ open class YJWaterfallFlowLayout: UICollectionViewFlowLayout {
     }
     
     open override func prepare() {
+        self.maxHeight = 0
         self.layoutAttributes.removeAll()
         self.headerLayoutAttributes.removeAll()
         self.cellLayoutAttributes.removeAll()
@@ -41,39 +42,45 @@ open class YJWaterfallFlowLayout: UICollectionViewFlowLayout {
             else {
                 return
         }
-        var sectionY: CGFloat = 0
         let sectionCount = dataSource.numberOfSections!(in: collectionView)
         for section in 0..<sectionCount {
             // header
-            let headerLA = self.prepareHeader(delegate: delegate, section: section, y: sectionY)
+            let headerLA = self.prepareHeader(delegate: delegate, section: section, y: self.maxHeight)
+            self.headerLayoutAttributes.append(headerLA)
+            self.layoutAttributes.append(headerLA)
+            self.maxHeight = headerLA.frame.maxY + self.sectionInset.top
             // cell
-            sectionY = headerLA.frame.maxY + self.sectionInset.top
-            let cellLAList = self.prepareCell(delegate: delegate, dataSource: dataSource, section: section, y: sectionY)
+            let cellLAList = self.prepareCell(delegate: delegate, dataSource: dataSource, section: section, y: self.maxHeight)
             self.cellLayoutAttributes.append(cellLAList)
             self.layoutAttributes.append(contentsOf: cellLAList)
             cellLAList.forEach { (la) in
-                sectionY = max(sectionY, la.frame.maxY)
+                self.maxHeight = max(self.maxHeight, la.frame.maxY)
             }
-            sectionY = sectionY + self.sectionInset.bottom
+            // bottom
+            self.maxHeight = self.maxHeight + self.sectionInset.bottom
         }
-        self.maxHeight = sectionY
     }
     
     func prepareHeader(delegate: UICollectionViewDelegateFlowLayout, section: Int, y: CGFloat) -> UICollectionViewLayoutAttributes {
-        var headerSize = self.headerReferenceSize
-        if let size = delegate.collectionView?(self.collectionView!, layout: self, referenceSizeForHeaderInSection: section) {
-            headerSize = size
-        }
+        let headerSize = self.sizeForHeader(delegate: delegate, section: section)
         let result = UICollectionViewLayoutAttributes(forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, with: IndexPath(item: 0, section: section))
         result.frame = CGRect(origin: CGPoint(x: 0, y: y), size: headerSize)
         return result
     }
     
+    func sizeForHeader(delegate: UICollectionViewDelegateFlowLayout, section: Int) -> CGSize {
+        if let headerSize = delegate.collectionView?(self.collectionView!, layout: self, referenceSizeForHeaderInSection: section) {
+            return headerSize
+        }
+        return self.headerReferenceSize
+    }
+    
     func prepareCell(delegate: UICollectionViewDelegateFlowLayout ,dataSource: UICollectionViewDataSource, section: Int, y: CGFloat) -> [UICollectionViewLayoutAttributes] {
         var result = [UICollectionViewLayoutAttributes]()
         let columnXList = self.prepareColumnXList()
-        var columnMaxYList = [CGFloat](repeating: y - self.minimumLineSpacing, count: self.columnCount)
-        var lowIndex = 0, highIndex = 0, columnIndex = 0
+        var columnYList = [CGFloat](repeating: y - self.minimumLineSpacing, count: self.columnCount)
+        var columnIndex = 0
+        var highLowIndex = (0, 0)
         let itemCount = dataSource.collectionView(self.collectionView!, numberOfItemsInSection: section)
         for item in 0..<itemCount {
             let indexPath = IndexPath(item: item, section: section)
@@ -83,20 +90,17 @@ open class YJWaterfallFlowLayout: UICollectionViewFlowLayout {
             let itemSize = self.sizeForItem(delegate: delegate, indexPath: indexPath)
             layoutAttributes.size = itemSize
             // origin
-            if itemSize.height <= columnMaxYList[highIndex] - columnMaxYList[lowIndex] {// 跳跃插入
-                layoutAttributes.frame.origin = CGPoint(x: columnXList[lowIndex], y: columnMaxYList[lowIndex] + self.minimumLineSpacing)
-                columnMaxYList[lowIndex] = layoutAttributes.frame.maxY
-                let highLowIndex = self.prepareHighLowIndex(columnMaxYList: columnMaxYList)
-                highIndex = highLowIndex.0
-                lowIndex = highLowIndex.1
+            let highIndex = highLowIndex.0, lowIndex = highLowIndex.1
+            if itemSize.height <= columnYList[highIndex] - columnYList[lowIndex] {// 跳跃插入
+                layoutAttributes.frame.origin = CGPoint(x: columnXList[lowIndex], y: columnYList[lowIndex] + self.minimumLineSpacing)
+                columnYList[lowIndex] = layoutAttributes.frame.maxY
+                highLowIndex = self.prepareHighLowIndex(columnYList: columnYList)
             } else {
-                layoutAttributes.frame.origin = CGPoint(x: columnXList[columnIndex], y: columnMaxYList[columnIndex] + self.minimumLineSpacing)
-                columnMaxYList[columnIndex] = layoutAttributes.frame.maxY
+                layoutAttributes.frame.origin = CGPoint(x: columnXList[columnIndex], y: columnYList[columnIndex] + self.minimumLineSpacing)
+                columnYList[columnIndex] = layoutAttributes.frame.maxY
                 columnIndex = columnIndex + 1
                 if columnIndex == self.columnCount - 1 { // 换行
-                    let highLowIndex = self.prepareHighLowIndex(columnMaxYList: columnMaxYList)
-                    highIndex = highLowIndex.0
-                    lowIndex = highLowIndex.1
+                    highLowIndex = self.prepareHighLowIndex(columnYList: columnYList)
                     columnIndex = 0
                 }
             }
@@ -116,27 +120,26 @@ open class YJWaterfallFlowLayout: UICollectionViewFlowLayout {
         return columnXArray
     }
     
-    func prepareHighLowIndex(columnMaxYList: [CGFloat]) -> (Int, Int) {
-        var highIndex = 0, lowIndex = 0
-        var height = columnMaxYList[0], low = columnMaxYList[0]
-        for i in 1..<columnMaxYList.count {
-            if columnMaxYList[i] > height {
-                highIndex = i
-                height = columnMaxYList[i]
-            } else if columnMaxYList[i] < low {
-                lowIndex = i
-                low = columnMaxYList[i]
+    func prepareHighLowIndex(columnYList: [CGFloat]) -> (Int, Int) {
+        var result = (0, 0)
+        var height = columnYList[0], low = columnYList[0]
+        for i in 1..<columnYList.count {
+            if columnYList[i] > height {
+                result.0 = i
+                height = columnYList[i]
+            } else if columnYList[i] < low {
+                result.1 = i
+                low = columnYList[i]
             }
         }
-        return (highIndex, lowIndex)
+        return result
     }
     
     func sizeForItem(delegate: UICollectionViewDelegateFlowLayout, indexPath: IndexPath) -> CGSize {
-        var itemSize = self.itemSize
-        if let size = delegate.collectionView?(self.collectionView!, layout: self, sizeForItemAt: indexPath) {
-            itemSize = size
+        if let itemSize = delegate.collectionView?(self.collectionView!, layout: self, sizeForItemAt: indexPath) {
+            return itemSize
         }
-        return itemSize
+        return self.itemSize
     }
     
 }
