a56a32065e5eeb6e2cab62e6d0ca9694bcf75aab
阳君 <937447974@qq.com>
2022-05-13 10:49:29 +0800

    数组升级

--- YJCocoa/Classes/AppFrameworks/Foundation/Extension/ArrayExt.swift
+++ YJCocoa/Classes/AppFrameworks/Foundation/Extension/ArrayExt.swift
@@ -11,30 +11,25 @@
 
 import UIKit
 
+public extension Optional where Wrapped == Array<Any> {
+    /// 判断数组是否为空
+    var isEmpty: Bool {
+        get { return self?.isEmpty ?? true }
+    }
+}
+
 public extension Array where Element: Any {
     
-    /// 按个数分割数组
-    /// - parameter count: 每组的个数
-    func split(count: Int) -> [[Element]] {
-        guard self.count > count else {
-            return [self]
-        }
-        var reslult = [[Element]]()
-        var group = [Element]()
-        for item in self {
-            group.append(item)
-            if group.count == count {
-                reslult.append(group)
-                group = [Element]()
-            }
-        }
-        if group.count != 0 { reslult.append(group) }
-        return reslult
+    /// 数据提取
+    /// - parameter index: 索引位置
+    func object(at index: Int) -> Element? {
+        guard index < self.count else { return nil }
+        return self[index]
     }
     
-    /// 将数组转换为字典
+    /// 按照key分组
     /// - parameter keySelector: 获取字典的key
-    func groupBy<K>(_ keySelector: (Element) -> K) -> [K: [Element]] where K: Any{
+    func groupBy<K>(_ keySelector: (Element) -> K) -> [K: [Element]] where K: Any {
         var result = [K: [Element]]()
         for item in self {
             let k = keySelector(item)
@@ -45,22 +40,37 @@ public extension Array where Element: Any {
         return result
     }
     
+    /// 转换为数组
+    /// - parameter transform: 转换器
+    func map<T>(_ transform: (Element) throws -> T?) rethrows -> [T] {
+        let mapArray: [T?] = try self.map(transform)
+        let filterArray: [T?] = mapArray.filter {  $0 != nil }
+        return filterArray.map{$0!}
+    }
+    
+    /// 转换为字典
+    /// - parameter keyTransform: key转换器
+    /// - parameter valueTransform: value转换器
+    func map<K, V>(_ keyTransform: (Element) throws -> K?, valueTransform: (Element) throws -> V? = {$0 as? V}) rethrows -> [K:V] {
+        var result = [K:V]()
+        for item in self {
+            if let key = try keyTransform(item) {
+                result[key] = try valueTransform(item)
+            }
+        }
+        return result
+    }
+    
+    /// 获取去重数组
+    func distinct() -> [Element] where Element: Equatable {
+        var result = [Element]()
+        for item in self {
+            if !result.contains(where: {$0 == item}) {
+                result.append(item)
+            }
+        }
+        return result
+    }
+    
 }
 
-public extension Array where Element == String {
-    func object(at index: Int) -> Element {
-        guard index < self.count else {
-            return ""
-        }
-        return self[index]
-    }
-}
-
-public extension Array where Element == Int {
-    func object(at index: Int) -> Element {
-        guard index < self.count else {
-            return 0
-        }
-        return self[index]
-    }
-}
