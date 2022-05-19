e26feb875a81a7332c6f09c7696f9a2a8e1ad452
阳君 <937447974@qq.com>
2022-05-19 09:04:04 +0800

    还原

--- YJCocoa/Classes/AppFrameworks/UIKit/PageView/YJUIPageViewCell.swift
+++ YJCocoa/Classes/AppFrameworks/UIKit/PageView/YJUIPageViewCell.swift
@@ -11,28 +11,16 @@
 
 import UIKit
 
-public protocol YJUIPageViewCellProtocol : UIViewController {
-    var cellObject: YJUIPageViewCellObject! { get }
-    var pageViewManager: YJUIPageViewManager! { get }
-    var cellDidAppear: YJUIPageViewCell.DidAppearClosure! { get set }
-    /// 获取 YJUITableCellObject
-    static func cellObject() -> YJUIPageViewCellObject
-    /// 获取 YJUITableCellObject 并自动填充模型
-    static func cellObject(withCellModel cellModel: Any) -> YJUIPageViewCellObject
-    /// 刷新 YJUIPageViewCell
-    func pageViewManager(_ pageViewManager: YJUIPageViewManager, reloadWith cellObject: YJUIPageViewCellObject)
-}
-
 /// PageView 的 cell 基类
 @objcMembers
-open class YJUIPageViewCell: UIViewController, YJUIPageViewCellProtocol {
+open class YJUIPageViewCell: UIViewController {
     
     /// 点击cell的回调
     public typealias DidAppearClosure = (_ index: Int) -> Void
     
     public private(set) var cellObject: YJUIPageViewCellObject!
     public private(set) var pageViewManager: YJUIPageViewManager!
-    public var cellDidAppear: DidAppearClosure!
+    var cellDidAppear: DidAppearClosure!
     
     // MARK: override
     open override func viewDidAppear(_ animated: Bool) {
